1c86a1e4b4f01fc131cc7e12f968fb220fe2ff7a
James Saryerwinnie <js@jamesls.com>
2016-07-07 14:02:55 -0700

    Add a few more examples in the generated new-project template
    Gives the user a better idea of what you can do without having to
    read the README.
    
    Closes #5.

--- chalice/cli/__init__.py
+++ chalice/cli/__init__.py
@@ -22,6 +22,28 @@ app = Chalice(app_name='%s')
 def index():
     return {'hello': 'world'}
 
+
+# The view function above will return {"hello": "world"}
+# whenver you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    {'hello': name}
+#
+# @app.route('/users/', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.json_body
+#     # Suppse we had some 'db' object that we used to
+#     # read/write from our database.
+#     # user_id = db.create_user(user_as_json)
+#     return {'user_id': user_id}
+#
+# See the README documentation for more examples.
+#
 """
 
 
