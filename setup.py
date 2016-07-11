ba657c48e0ef128e6b17b403f6751789ed197d14
James Saryerwinnie <js@jamesls.com>
2016-07-11 13:21:19 -0700

    Update trove classifiers
    Chalice supports 2.7, which is the only version supported by
    AWS Lambda.

--- setup.py
+++ setup.py
@@ -39,11 +39,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 )
