de15d5e04c5e3a267bb4e153c296e1ed2ab33e9e
James Saryerwinnie <js@jamesls.com>
2016-07-22 17:14:04 +0200

    Set region when creating clients
    This fixes a travis failure.

--- tests/unit/test_deployer.py
+++ tests/unit/test_deployer.py
@@ -33,7 +33,8 @@ def stubbed_client(service_name):
     global _SESSION
     if _SESSION is None:
         _SESSION = botocore.session.get_session()
-    client = _SESSION.create_client(service_name)
+    client = _SESSION.create_client(service_name,
+                                    region_name='us-west-2')
     stubber = Stubber(client)
     return client, stubber
 
