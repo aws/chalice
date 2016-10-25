0b9a4b7890916338d43bdb5cf53c72dcd4aad3e5
James Saryerwinnie <js@jamesls.com>
2016-10-24 21:53:55 -0700

    Initial support for multi file apps
    There's been several approaches proposed:
    
    1. Support either 'app.py' *or* an ``app/`` directory.
    2. Recursively find all python modules/packages (similar to what
       ``find_packages(.)`` does.
    3. Have some type of manifest file that includes/excludes files.
    4. Include a hard coded directory if it exists.
    
    This commit implements option 4, which I think has a good balance
    of control vs. ease of use:
    
    * If you want something included in the deployment package, put it
      in this directory.  This works whether it's a python file or not.
    * If you don't something included in your deployment package, don't
      include it in ``chalicelib/``.

--- chalice/cli/__init__.py
+++ chalice/cli/__init__.py
@@ -5,6 +5,7 @@ Contains commands for deploying chalice.
 """
 import os
 import json
+import sys
 import logging
 
 import click
@@ -91,6 +92,8 @@ def load_project_config(project_dir):
 
 def load_chalice_app(project_dir):
     app_py = os.path.join(project_dir, 'app.py')
+    if project_dir not in sys.path:
+        sys.path.append(project_dir)
     with open(app_py) as f:
         g = {}
         contents = f.read()
