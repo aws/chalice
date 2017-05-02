ac9ee17f93b90a79b629d222d8c2846debed6f04
James Saryerwinnie <js@jamesls.com>
2017-05-01 17:39:23 -0700

    Add authorizers as top level import

--- chalice/__init__.py
+++ chalice/__init__.py
@@ -1,7 +1,8 @@
 from chalice.app import Chalice
 from chalice.app import (
     ChaliceViewError, BadRequestError, UnauthorizedError, ForbiddenError,
-    NotFoundError, ConflictError, TooManyRequestsError, Response, CORSConfig
+    NotFoundError, ConflictError, TooManyRequestsError, Response, CORSConfig,
+    CustomAuthorizer, CognitoUserPoolAuthorizer
 )
 
 __version__ = '0.8.0'
