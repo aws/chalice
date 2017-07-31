1963d144362a66ca39ffdb909163ef4301a8048d
James Saryerwinnie <js@jamesls.com>
2017-07-31 15:49:51 -0700

    Change status from Beta to Production/Stable

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         ]
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
