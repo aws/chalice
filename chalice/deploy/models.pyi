dadaee96d1ebbb2bdfbf7ce3858a6a2701afd24c
James Saryerwinnie <js@jamesls.com>
2017-09-14 20:36:31 -0700

    Initial POC of new deployer
    The existing deployer module is starting to get out of hand.
    Initially it only had to handle API gateway and a single Lambda
    function, but over time we've added support for AWS resources,
    and continue to add additional resources (#516).
    
    I don't think the existing deployer design is going to hold up
    as all these new resources are being added.  The code is hard to
    understand, hard to use, and hard to modify.  There's also a number
    of reported customer reported bugs from the existing deployer.
    
    Digging further into the deployer code, there are a number of
    inconsistencies as well as unexpected and unintuitive behaviors.
    
    I've been experimenting with a new redesign of the deployer to
    not only fix the existing issues, but also make future changes
    easier.  This initial POC only supports `@app.lambda_function()`
    and only sketches out how to do the initial creation of resources,
    but I think this should be able to handle the remaining gaps.
    
    To help with initial testing, I've added a temporary `deploy-new`
    command so both deployers can live side by side during development.
    Before this gets merged to master, `deploy-new` will just become
    `deploy` and the old code will be removed.
    
    The `newdeployer` module docstring has a description of how
    the new system works, and is a good starting point for a technical
    overview.

---
+++ chalice/deploy/models.pyi
@@ -0,0 +1,124 @@
+from typing import List, Dict, Any, TypeVar, Union
+import enum
+
+class DeployPhase(enum.Enum):
+    BUILD = 'build'
+    DEPLOY = 'deploy'
+
+
+T = TypeVar('T')
+DV = Union[DeployPhase, T]
+STR_MAP = Dict[str, str]
+
+
+class Model:
+    def dependencies(self) -> List[Model]: ...
+
+
+class ManagedModel(Model):
+    resource_name = ...  # type: str
+    resource_type = ...  # type: str
+
+
+class Application(Model):
+    stage = ...  # type: str
+    resources = ... # type: List[Model]
+
+    def __init__(self,
+                 stage,       # type: str
+                 resources,   # type: List[Model]
+                 ):
+        # type: (...) -> None
+        ...
+
+
+class DeploymentPackage(Model):
+    filename = ...  # type: str
+
+    def __init__(self,
+                 filename,   # type: DV[str]
+                 ):
+        # type: (...) -> None
+        ...
+
+
+class IAMPolicy(Model):
+    pass
+
+
+class FileBasedIAMPolicy(IAMPolicy):
+    filename = ...  # type: str
+
+    def __init__(self,
+                 filename,   # type: str
+                 ):
+        # type: (...) -> None
+        ...
+
+
+class AutoGenIAMPolicy(IAMPolicy):
+    document = ...  # type: DV[Dict[str, Any]]
+
+    def __init__(self,
+                 document,   # type: DV[Dict[str, Any]]
+                 ):
+        # type: (...) -> None
+        ...
+
+
+class IAMRole(Model):
+    pass
+
+
+class PreCreatedIAMRole(IAMRole):
+    role_arn = ... # type: str
+
+    def __init__(self,
+                 role_arn,   # type: str
+                 ):
+        # type: (...) -> None
+        ...
+
+
+class ManagedIAMRole(IAMRole, ManagedModel):
+    role_arn = ... # type: DV[str]
+    role_name = ... # type: str
+    trust_policy = ... # type: Dict[str, Any]
+    policy = ... # type: IAMPolicy
+
+    def __init__(self,
+                 resource_name,  # type: str
+                 role_arn,       # type: DV[str]
+                 role_name,      # type: str
+                 trust_policy,   # type: Dict[str, Any]
+                 policy,         # type: IAMPolicy
+                 ):
+        # type: (...) -> None
+        ...
+
+
+class LambdaFunction(ManagedModel):
+    function_name = ... # type: str
+    deployment_package = ... # type: DeploymentPackage
+    environment_variables = ... # type: STR_MAP
+    runtime = ... # type: str
+    handler = ... # type: str
+    tags = ... # type: STR_MAP
+    timeout = ... # type: int
+    memory_size = ... # type: int
+    role = ... # type: IAMRole
+
+    def __init__(self,
+                 resource_name,           # type: str
+                 function_name,           # type: str
+                 deployment_package,      # type: DeploymentPackage
+                 environment_variables,   # type: STR_MAP
+                 runtime,                 # type: str
+                 handler,                 # type: str
+                 tags,                    # type: STR_MAP
+                 timeout,                 # type: int
+                 memory_size,             # type: int
+                 role,                    # type: IAMRole
+                 ):
+        # type: (...) -> None
+        ...
