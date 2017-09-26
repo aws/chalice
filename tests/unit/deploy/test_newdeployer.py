23dc06bc8de9abe3a5e36038331c1f03911ac9eb
James Saryerwinnie <js@jamesls.com>
2017-09-26 10:44:23 -0700

    Initial commit of updating resources
    This is the bare minimum changes need to support updating.
    A subsequent commit will refactor this into a more generalized
    pattern.

--- tests/unit/deploy/test_newdeployer.py
+++ tests/unit/deploy/test_newdeployer.py
@@ -481,7 +481,7 @@ class TestDeploymentPackager(object):
         assert not generator.create_deployment_package.called
 
 
-class TestPlanStage(object):
+class TestPlanStageCreate(object):
     def test_can_plan_for_iam_role_creation(self, mock_client, mock_osutils):
         mock_client.get_role_arn_for_name.side_effect = \
             ResourceDoesNotExistError()
@@ -503,20 +503,28 @@ class TestPlanStage(object):
         assert api_call.target_variable == 'myrole_role_arn'
         assert api_call.resource == resource
 
-    def test_no_action_if_role_exists(self, mock_client, mock_osutils):
-        # This might need some tweaking to figure out if we need
-        # to update the policy or not.
-        mock_client.get_role_arn_for_name.return_value = 'role:arn'
-        planner = PlanStage(mock_client, mock_osutils)
+    def test_can_create_plan_for_filebased_role(self, mock_client,
+                                                mock_osutils):
+        mock_client.get_role_arn_for_name.side_effect = \
+                ResourceDoesNotExistError
         resource = models.ManagedIAMRole(
             resource_name='default-role',
             role_arn=models.Placeholder.DEPLOY_STAGE,
             role_name='myrole',
             trust_policy={'trust': 'policy'},
-            policy=models.AutoGenIAMPolicy(document={'iam': 'policy'}),
+            policy=models.FileBasedIAMPolicy(filename='foo.json'),
         )
-        plan = planner.execute(Config.create(), [resource])
-        assert plan == []
+        mock_osutils.get_file_contents.return_value = '{"iam": "policy"}'
+        planner = PlanStage(mock_client, mock_osutils)
+        plan = planner.execute(Config.create(project_dir='.'), [resource])
+        assert len(plan) == 1
+        api_call = plan[0]
+        assert api_call.method_name == 'create_role'
+        assert api_call.params == {'name': 'myrole',
+                                   'trust_policy': {'trust': 'policy'},
+                                   'policy': {'iam': 'policy'}}
+        assert api_call.target_variable == 'myrole_role_arn'
+        assert api_call.resource == resource
 
     def test_can_create_function(self, mock_client, mock_osutils):
         mock_client.lambda_function_exists.return_value = False
@@ -540,16 +548,10 @@ class TestPlanStage(object):
         }
         assert call.resource == function
 
-    def test_no_plan_if_function_exists(self, mock_client, mock_osutils):
-        mock_client.lambda_function_exists.return_value = True
-        function = create_function_resource('function_name')
-        planner = PlanStage(mock_client, mock_osutils)
-        plan = planner.execute(Config.create(), [function])
-        # This will change once updates are implemented.
-        assert plan == []
-
     def test_can_create_plan_for_managed_role(self, mock_client, mock_osutils):
         mock_client.lambda_function_exists.return_value = False
+        mock_client.get_role_arn_for_name.side_effect = \
+            ResourceDoesNotExistError
         function = create_function_resource('function_name')
         function.role = models.ManagedIAMRole(
             resource_name='myrole',
@@ -570,6 +572,120 @@ class TestPlanStage(object):
         role_arn = call.params['role_arn']
         assert isinstance(role_arn, Variable)
         assert role_arn.name == 'myrole-dev_role_arn'
+
+
+class TestPlanStageUpdate(object):
+    def test_can_update_lambda_function_code(self, mock_client, mock_osutils):
+        mock_client.lambda_function_exists.return_value = True
+        function = create_function_resource('function_name')
+        # Now let's change the memory size and ensure we
+        # get an update.
+        function.memory_size = 256
+        planner = PlanStage(mock_client, mock_osutils)
+        plan = planner.execute(Config.create(), [function])
+        assert len(plan) == 1
+        call = plan[0]
+        assert call.method_name == 'update_function'
+        assert call.resource == function
+        # We don't need to set a target variable because the
+        # function already exists and we know the arn.
+        assert call.target_variable is None
+        existing_params = {
+            'function_name': 'appname-dev-function_name',
+            'role_arn': 'role:arn',
+            'zip_contents': mock.ANY,
+            'runtime': 'python2.7',
+            'environment_variables': {},
+            'tags': {},
+            'timeout': 60,
+        }
+        expected = dict(memory_size=256, **existing_params)
+        assert call.params == expected
+
+    def test_can_update_managed_role(self, mock_client, mock_osutils):
+        mock_client.get_role_arn_for_name.return_value = 'myrole:arn'
+        role = models.ManagedIAMRole(
+            resource_name='resource_name',
+            role_arn='myrole:arn',
+            role_name='myrole',
+            trust_policy={},
+            policy=models.AutoGenIAMPolicy(document={'role': 'policy'}),
+        )
+        planner = PlanStage(mock_client, mock_osutils)
+        plan = planner.execute(Config.create(), [role])
+        assert len(plan) == 2
+        delete_call = plan[0]
+        assert delete_call.method_name == 'delete_role_policy'
+        assert delete_call.params == {'role_name': 'myrole',
+                                      'policy_name': 'myrole'}
+        assert delete_call.resource == role
+
+        update_call = plan[1]
+        assert update_call.method_name == 'put_role_policy'
+        assert update_call.params == {'role_name': 'myrole',
+                                      'policy_name': 'myrole',
+                                      'policy_document': {'role': 'policy'}}
+        assert update_call.resource == role
+
+    def test_can_update_file_based_policy(self, mock_client, mock_osutils):
+        mock_client.get_role_arn_for_name.return_value = 'myrole:arn'
+        role = models.ManagedIAMRole(
+            resource_name='resource_name',
+            role_arn='myrole:arn',
+            role_name='myrole',
+            trust_policy={},
+            policy=models.FileBasedIAMPolicy(filename='foo.json'),
+        )
+        mock_osutils.get_file_contents.return_value = '{"iam": "policy"}'
+        planner = PlanStage(mock_client, mock_osutils)
+        plan = planner.execute(Config.create(), [role])
+        assert len(plan) == 2
+        delete_call = plan[0]
+        assert delete_call.method_name == 'delete_role_policy'
+        assert delete_call.params == {'role_name': 'myrole',
+                                      'policy_name': 'myrole'}
+        assert delete_call.resource == role
+
+        update_call = plan[1]
+        assert update_call.method_name == 'put_role_policy'
+        assert update_call.params == {'role_name': 'myrole',
+                                      'policy_name': 'myrole',
+                                      'policy_document': {'iam': 'policy'}}
+        assert update_call.resource == role
+
+    def test_no_update_for_non_managed_role(self):
+        role = models.PreCreatedIAMRole(role_arn='role:arn')
+        planner = PlanStage(mock_client, mock_osutils)
+        plan = planner.execute(Config.create(), [role])
+        assert plan == []
+
+    def test_can_update_with_placeholder_but_exists(self, mock_client,
+                                                    mock_osutils):
+        mock_client.get_role_arn_for_name.return_value = 'myrole:arn'
+        role = models.ManagedIAMRole(
+            resource_name='resource_name',
+            role_arn=models.Placeholder.DEPLOY_STAGE,
+            role_name='myrole',
+            trust_policy={},
+            policy=models.AutoGenIAMPolicy(document={'role': 'policy'}),
+        )
+        planner = PlanStage(mock_client, mock_osutils)
+        plan = planner.execute(Config.create(), [role])
+        assert len(plan) == 2
+        delete_call = plan[0]
+        assert delete_call.method_name == 'delete_role_policy'
+        assert delete_call.params == {'role_name': 'myrole',
+                                      'policy_name': 'myrole'}
+        assert delete_call.resource == role
+
+        update_call = plan[1]
+        assert update_call.method_name == 'put_role_policy'
+        assert update_call.params == {'role_name': 'myrole',
+                                      'policy_name': 'myrole',
+                                      'policy_document': {'role': 'policy'}}
+        assert update_call.resource == role
+
+        assert role.role_arn == 'myrole:arn'
 
 
 class TestInvoker(object):
