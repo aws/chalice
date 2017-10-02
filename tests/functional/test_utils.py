b12e2a9f7f60c33f94cf15be65cb1a6e871067c7
James Saryerwinnie <js@jamesls.com>
2017-10-02 14:34:15 -0700

    Incorporate James's feedback

--- tests/functional/test_utils.py
+++ tests/functional/test_utils.py
@@ -112,9 +112,9 @@ class TestOSUtils(object):
     def test_can_read_unicode(self, tmpdir, osutils):
         filename = str(tmpdir.join('file.txt'))
         checkmark = u'\2713'
-        with io.open(filename, 'w', encoding='utf-8') as f:
+        with io.open(filename, 'w', encoding='utf-16') as f:
             f.write(checkmark)
 
         content = osutils.get_file_contents(filename, binary=False,
-                                            encoding='utf-8')
+                                            encoding='utf-16')
         assert content == checkmark
