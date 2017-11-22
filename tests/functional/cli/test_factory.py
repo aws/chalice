a76b5c6b6bc0a82eb67714c6414530bf87a1acef
Bubba <rob@stechstudio.com>
2017-11-22 13:14:43 -0500

    Test we can create with stage specific env vars.

--- tests/functional/cli/test_factory.py
+++ tests/functional/cli/test_factory.py
@@ -91,6 +91,29 @@ def test_can_create_config_obj_with_default_env_vars(setup_app_dir):
     obj = factori.create_config_obj(env=env)
     assert env['GLOBAL_FOO'] == 'global_bar'
     assert isinstance(obj, Config)
+
+
+def test_can_create_config_obj_with_stage_env_vars(setup_app_dir):
+    config = {
+        "version": "2.0",
+        "app_name": "replaceme",
+        "stages": {
+            "dev": {
+                "api_gateway_stage": "api",
+                "environment_variables": {
+                    "DEV_FOO": "dev_bar"
+                }
+            }
+        }
+    }
+    env = {}
+    setup_chalice_dir(setup_app_dir, config=json.dumps(config))
+    factori = factory.CLIFactory(str(setup_app_dir))
+    obj = factori.create_config_obj(env=env)
+    assert env['DEV_FOO'] == 'dev_bar'
+    assert isinstance(obj, Config)
+
+
 def test_can_create_config_obj_default_autogen_policy_true(clifactory):
     config = clifactory.create_config_obj()
     assert config.autogen_policy is True
