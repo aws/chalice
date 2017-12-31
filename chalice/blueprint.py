955b6f7df35bd9a0ef7abd4ff1db6006c0c41c1e
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-12-31 15:50:39 -0500

    support current_request property in blueprints

--- chalice/blueprint.py
+++ chalice/blueprint.py
@@ -1,9 +1,17 @@
 
 class Blueprint:
 
+    REGISTERED_APP = None
+
     def __init__(self, name):
         self.name = name
         self.routes = []
+
+    @property
+    def current_request(self):
+        if self.REGISTERED_APP is None:
+            raise RuntimeError('Blueprint must be registered first!')
+        return self.REGISTERED_APP.current_request
 
     def route(self, path, **kwargs):
         def _register_view(view_func):
