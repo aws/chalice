78aee05eae37d7f5692a6d296f5838b44943e3f3
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-12-31 16:12:49 -0500

    implement url_for on blueprints

--- chalice/blueprint.py
+++ chalice/blueprint.py
@@ -7,6 +7,21 @@ class Blueprint:
         self.name = name
         self.routes = []
 
+    def url_for(self, view_name):
+        if view_name.startswith('{}.'.format(self.name)):
+            pass
+        else:
+            view_name = '{}.{}'.format(self.name, view_name.lstrip('.'))
+
+        if self.REGISTERED_APP is None:
+            raise RuntimeError('Blueprint must be registered first!')
+        for path, methods in self.REGISTERED_APP.routes.items():
+            for method in methods.keys():
+                if self.REGISTERED_APP.routes[path][method].view_name == view_name:
+                    return path
+
+        raise LookupError('No url found for {}'.format(view_name))
+
     @property
     def current_request(self):
         if self.REGISTERED_APP is None:
