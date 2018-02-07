811d5d9e5eac3a7458eab315663fc92d20e3e70e
Victor Titor <vtitor.edumix@gmail.com>
2018-02-07 16:48:30 +0200

    Add type annotation

--- chalice/cli/reload.py
+++ chalice/cli/reload.py
@@ -6,33 +6,43 @@ import time
 import types
 
 import six
+from typing import Optional, Dict, Type
 
 from chalice.constants import AUTORELOAD_INTERVAL
 
 
 class Reloader(threading.Thread):
     def __init__(self, autoreload=True):
+        # type: (bool) -> None
         super(Reloader, self).__init__()
         self.autoreload = autoreload
         self.triggered = False
-        self.mtimes = {}
+        self.mtimes = {}  # type: Dict[str, float]
 
     def __enter__(self):
+        # type: () -> Reloader
         if self.autoreload:
             self.setDaemon(True)
             self.start()
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self,
+                 exc_type,  # type: Optional[Type[BaseException]]
+                 exc_value,  # type: Optional[BaseException]
+                 traceback  # type: Optional[types.TracebackType]
+                 ):
+        # type: (...) -> Optional[bool]
         if exc_type is KeyboardInterrupt and self.triggered:
             sys.exit(1)
 
     def run(self):
+        # type: () -> None
         while True:
             time.sleep(AUTORELOAD_INTERVAL)
             if self.find_changes():
                 self.reload()
 
     def find_changes(self):
+        # type: () -> bool
         for module in list(sys.modules.values()):
             if not isinstance(module, types.ModuleType):
                 continue
@@ -48,8 +58,10 @@ class Reloader(threading.Thread):
             old_time = self.mtimes.setdefault(path, mtime)
             if mtime > old_time:
                 return True
+        return False
 
     def reload(self):
+        # type: () -> None
         self.triggered = True
         if sys.platform == 'win32':
             subprocess.Popen(sys.argv, close_fds=True)
