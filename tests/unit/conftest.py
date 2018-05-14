3f4ba597274f47d7ccf48abf665ae8edcd6fa7e2
James Saryerwinnie <js@jamesls.com>
2018-05-14 10:32:07 -0700

    Skip healthcheck too slow when running on travis
    Example of failing build: https://travis-ci.org/aws/chalice/jobs/377921987
    Docs: http://hypothesis.readthedocs.io/en/latest/healthchecks.html#hypothesis.HealthCheck

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,14 +2,17 @@ import json
 import os
 
 from pytest import fixture
-from hypothesis import settings
+from hypothesis import settings, HealthCheck
 
 from chalice.app import Chalice
 
 # From:
 # http://hypothesis.readthedocs.io/en/latest/settings.html#settings-profiles
 # On travis we'll have it run through more iterations.
-settings.register_profile('ci', settings(max_examples=2000))
+settings.register_profile(
+    'ci', settings(max_examples=2000,
+                   suppress_health_check=[HealthCheck.too_slow]),
+)
 # When you're developing locally, we'll only run a few examples
 # to keep unit tests fast.  If you want to run more iterations
 # locally just set HYPOTHESIS_PROFILE=ci.
