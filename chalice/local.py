b5b57b3adf87be22b43c9d93f60ba95fac42b917
Bohui Han <hanhanab@gmail.com>
2018-09-14 16:12:46 +1000

    Return value could be none or dictionary

--- chalice/local.py
+++ chalice/local.py
@@ -165,7 +165,7 @@ class LambdaEventConverter(object):
         return headers.get('content-type', '') in self._binary_types
 
     def _get_local_context(self, headers):
-        # type: (Dict[str,str]) -> Dict[str,str]
+        # type: (Dict[str,str]) -> Any
         local_context = headers.pop('local-request-context', "")
         if not local_context:
             return None
