d6639c079fc323409c3e84434688929057f9c825
sean-ahn <sean9509@gmail.com>
2019-01-21 17:33:12 +0900

    Ignore typing when python_version is 3.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as readme_file:
 install_requires = [
     'click>=6.6,<7.0',
     'botocore>=1.10.48,<2.0.0',
-    'typing==3.6.4',
+    'typing==3.6.4;python_version<'3.7'',
     'six>=1.10.0,<2.0.0',
     'pip>=9,<=18.1',
     'attrs==17.4.0',
