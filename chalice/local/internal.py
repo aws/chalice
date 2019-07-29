76b67871b644192b9c4871bcfeebe2055e0c53f3
studio3104 <studio3104.com@gmail.com>
2019-07-29 11:40:12 +0900

    Transplant implementations from `pytest-chalice`

---
+++ chalice/local/internal.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+This is intended only for `pytest-chalice`.
+https://github.com/studio3104/pytest-chalice
+
+"""
+
+import json
+import re
+
+from logging import getLogger
+
+from chalice import Chalice
+from chalice.config import Config
+from chalice.local import LocalGateway
+
+from typing import Any, Callable, Dict
+
+
+logger = getLogger(__name__)
+
+# Python 3.4 or older don't have JSONDecodeError within json module
+JSONDecodeError = ValueError  # type: ignore
+if hasattr(json, 'JSONDecodeError'):
+    JSONDecodeError = json.JSONDecodeError  # type: ignore
+
+
+class InternalLocalGateway(LocalGateway):
+    def __init__(self, *args, **kwargs):
+        # type: (Any, Any) -> None
+        self.custom_context = {}  # type: Dict
+        super(InternalLocalGateway, self).__init__(*args, **kwargs)
+
+    @property
+    def custom_context(self):
+        # type: () -> Dict[str, Any]
+        return self.__custom_context
+
+    @custom_context.setter
+    def custom_context(self, context):
+        # type: (Dict[str, Any]) -> None
+        self.__custom_context = context
+
+    def _generate_lambda_event(self, *args, **kwargs):
+        # type: (Any, Any) -> Dict[str, Any]
+        event = super(InternalLocalGateway, self)._generate_lambda_event(*args, **kwargs)
+        event['requestContext'].update(self.custom_context)
+        return event
+
+
+UPPERCASE_PATTERN = re.compile('([A-Z])')
+
+
+class ResponseHandler(object):
+    def __init__(self, values):
+        # type: (Dict[str, Any]) -> None
+        self.values = {}  # type: Dict
+
+        for key, value in values.items():
+            snake_key = re.sub(UPPERCASE_PATTERN, lambda x: '_' + x.group(1).lower(), key)
+            self.values[snake_key] = value
+
+        try:
+            self.values['json'] = json.loads(self.values['body'])
+        except JSONDecodeError:  # type: ignore
+            logger.info('Response body is NOT JSON decodable: {}'.format(self.values['body']))
+
+    def __getattr__(self, key):
+        # type: (str) -> Any
+        try:
+            return self.values[key]
+        except KeyError:
+            raise AttributeError("'{}' object has no attribute '{}'".format(self.__class__.__name__, key))
+
+
+class RequestHandler(object):
+    METHODS = ('get', 'head', 'post', 'options', 'put', 'delete', 'trace', 'patch', 'link', 'unlink')
+
+    def __init__(self, app):
+        # type: (Chalice) -> None
+        self.local_gateway = InternalLocalGateway(app, Config())
+
+    @property
+    def custom_context(self):
+        # type: () -> Dict[str, Any]
+        return self.local_gateway.custom_context
+
+    # As of Chalice version 1.8.0,
+    # LocalGateway object doesn't handle Cognito's context like as the warning message below shows,
+    #
+    # "UserWarning: CognitoUserPoolAuthorizer is not a supported in local mode.
+    # All requests made against a route will be authorized to allow local testing."
+    #
+    # Not only for this purpose, it's an interface provided to allow custom contexts in unit tests.
+    @custom_context.setter
+    def custom_context(self, context):
+        # type: (Dict[str, Any]) -> None
+        self.local_gateway.custom_context = context
+
+    def __getattr__(self, method):
+        # type: (str) -> Callable
+        if method not in self.METHODS:
+            raise AttributeError(
+                "'{}' object has no attribute '{}'".format(
+                    self.__class__.__name__, method))
+
+        def request(path, headers={}, body=''):
+            # type: (str, Dict[str, str], str) -> ResponseHandler
+            response = self.local_gateway.handle_request(
+                method=method.upper(), path=path, headers=headers, body=body)
+            return ResponseHandler(response)
+
+        return request
---
+++ tests/unit/local/test_internal.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+from typing import Any, Dict, Iterator, Union
+
+import pytest
+
+from chalice import Chalice
+from chalice.local.internal import RequestHandler
+
+
+@pytest.fixture
+def sample_app():
+    # type: () -> Iterator[Union[Iterator, Iterator[Chalice]]]
+    app = Chalice(__name__)
+
+    @app.route('/', methods=('GET', 'HEAD', 'POST', 'OPTIONS', 'PUT', 'DELETE', 'TRACE', 'PATCH', 'LINK', 'UNLINK'))
+    def index():
+        # type: () -> Dict[str, str]
+        return {'hello': 'world'}
+
+    @app.route('/context')
+    def context():
+        # type: () -> Dict[str, Dict[str, Any]]
+        context = app.current_request.context
+        return {'context': context}
+
+    @app.route('/string')
+    def string():
+        # type: () -> str
+        return 'Foo'
+
+    yield app
+
+
+@pytest.fixture
+def sample_client(sample_app):
+    # type: (Chalice) -> RequestHandler
+    return RequestHandler(sample_app)
+
+
+class TestRequestHandler:
+    @pytest.mark.parametrize('method',  (
+        'get', 'head', 'post', 'put', 'delete', 'trace', 'patch', 'link', 'unlink',
+    ))
+    def test_json_response(self, method, sample_client):
+        response = getattr(sample_client, method)('/')
+        assert response.status_code == 200
+        assert response.json == {'hello': 'world'}
+
+    def test_invalid_method(self, sample_client):
+        # type: (RequestHandler) -> None
+        with pytest.raises(AttributeError, match=r' object has no attribute '):
+            sample_client.invalid_method('/')
+
+    def test_string_response_dont_have_json_attribute(self, sample_client):
+        # type: (RequestHandler) -> None
+        response = sample_client.get('/string')
+        assert not hasattr(response, 'json')
+
+
+class TestCustomContext:
+    def test_check_default_context(self, sample_client):
+        # type: (RequestHandler) -> None
+        response = sample_client.get('/context')
+        assert response.json == {
+            'context': {
+                'httpMethod': 'GET',
+                'identity': {'sourceIp': '127.0.0.1'},
+                'path': '/context',
+                'resourcePath': '/context',
+            }
+        }
+
+    def test_custom_context(self, sample_client):
+        # type: (RequestHandler) -> None
+        sample_client.custom_context = {
+            'authorizer': {'claims': {}},
+        }
+
+        response = sample_client.get('/context')
+        response_context = response.json['context']
+        assert 'httpMethod' in response_context
+        assert 'identity' in response_context
+        assert 'path' in response_context
+        assert 'resourcePath' in response_context
+        assert 'authorizer' in response_context
