0966af6bf5cdddad930ec56a3ecb15cda971cb93
Grant Soyka <gsoyka@gmail.com>
2019-08-23 15:36:45 -0600

    Don't create cache directory

--- tests/functional/cli/test_factory.py
+++ tests/functional/cli/test_factory.py
@@ -32,8 +32,6 @@ def clifactory(tmpdir):
     )
     chalice_dir = appdir.mkdir('.chalice')
     chalice_dir.join('config.json').write('{}')
-    if not os.path.exists('~/.aws/cli/cache'):
-        os.makedirs(os.path.expanduser('~/.aws/cli/cache'))
     return factory.CLIFactory(str(appdir))
 
 
