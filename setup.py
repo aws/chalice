21a23bbcdb1dad889beb33b45e17017050b2bff9
Gábor Lipták <gliptak@gmail.com>
2019-10-01 20:21:41 -0400

    Update Python versions in setup
    Signed-off-by: Gábor Lipták <gliptak@gmail.com>

--- setup.py
+++ setup.py
@@ -50,5 +50,6 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
