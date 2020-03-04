3b5fb06055411e7cf0214432086cd26557b48562
James Saryerwinnie <js@jamesls.com>
2020-03-04 18:10:06 -0500

    Make ws integ test more robust
    * Bump up provisioned throughput.  A sleep time of 0.05
      is a TPS of 20, so to be safe I set the throughput to 50.
    * Catch and report any errors in the lambda function.  That way
      we have a better idea if anything fails.  Also assert that there
      were no errors in the lambda function.
    * Fix bug where the counter would send a value of `None` instead of
      an integer.

--- tests/aws/testwebsocketapp/app.py
+++ tests/aws/testwebsocketapp/app.py
@@ -13,11 +13,28 @@ ddb = boto3.client('dynamodb')
 
 @app.on_ws_message()
 def message(event):
-    ddb.put_item(
-        TableName=os.environ['APP_NAME'],
-        Item={
-            'entry': {
-                'N': event.body
+    try:
+        ddb.put_item(
+            TableName=os.environ['APP_NAME'],
+            Item={
+                'entry': {
+                    'N': event.body
+                },
             },
-        },
-    )
+        )
+    except Exception as e:
+        # If we get an exception, we need to log it somehow.  We can't
+        # return this back to the user so we'll add something to the ddb
+        # table to denote that we failed.
+        ddb.put_item(
+            TableName=os.environ['APP_NAME'],
+            Item={
+                'entry': {
+                    'N': "-9999"
+                },
+                'errormsg': {
+                    'S': '%s: %s,\noriginal event: %s' % (
+                        e.__class__, e, event.to_dict())
+                }
+            }
+        )
