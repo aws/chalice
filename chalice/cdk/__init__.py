16da4e047da2fafd032cfc8d052d0edeb709ebdb
James Saryerwinnie <js@jamesls.com>
2021-01-25 15:04:30 -0500

    Add clarifying notes on CDK integration

--- chalice/cdk/__init__.py
+++ chalice/cdk/__init__.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     warnings.warn('Unable to import the Chalice CDK construct due to missing '
                   'dependencies.\nYou can install these by running '
-                  "'pip install chalice[cdk]'")
+                  "'pip install \"chalice[cdk]\"'")
 
 
 __all__ = ['Chalice']
