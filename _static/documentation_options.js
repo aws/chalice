b1272247f8e48759103a61c18cbc0a3a7443cf83
github-aws-sdk-python-automation@amazon.com <aws-sdk-python-automation>
2022-01-19 18:52:52 +0000

    Updating generated documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.26.4',
+    VERSION: '1.26.5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
