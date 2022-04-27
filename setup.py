b092963f565e33246ecf3053605fa4bf2ecdbf35
Daniel Sotirhos <dsotirho@ucsc.edu>
2022-04-27 14:11:01 -0700

    Version 1.27.0+8

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ install_requires = [
 
 setup(
     name='chalice',
-    version='1.27.0',
+    version='1.27.0+8',
     description="Microframework",
     long_description=README,
     author="James Saryerwinnie",
