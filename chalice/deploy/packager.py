378e78cba61ccbaede1196b07b1e7311a2fd2131
jonathan343 <jonathangee09@gmail.com>
2025-05-12 15:23:41 -0400

    Add support for Python 3.13
    Cherry-picks commit from original PR for Python 3.13 support
    (#2137)

--- chalice/deploy/packager.py
+++ chalice/deploy/packager.py
@@ -81,6 +81,7 @@ class BaseLambdaDeploymentPackager(object):
         'python3.10': 'cp310',
         'python3.11': 'cp311',
         'python3.12': 'cp312',
+        'python3.13': 'cp313',
     }
 
     def __init__(
@@ -498,7 +499,8 @@ class DependencyBuilder(object):
         'cp38': (2, 26),
         'cp310': (2, 26),
         'cp311': (2, 26),
-        'cp312': (2, 26),
+        'cp312': (2, 34),
+        'cp313': (2, 34),
     }
     # Fallback version if we're on an unknown python version
     # not in _RUNTIME_GLIBC.
